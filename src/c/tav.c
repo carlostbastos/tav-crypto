eeb699aa0c85001a43ce9f16b64d12a94533f5da
Carlos Alberto Terencio de Bastos <caterencio@yahoo.com.br>
2025-11-26 19:54:53 -0300

    Update tav.c

--- src/c/tav.c
+++ src/c/tav.c
@@ -1,9 +1,12 @@
 /*
+ * TAV Clock Cryptography v9.1
+ * Copyright (C) 2025 Carlos Alberto Terencio de Bastos
+ * License: AGPL-3.0 - https://github.com/caterencio/tav-crypto
+ */
+
+/*
  * TAV CLOCK CRYPTOGRAPHY V9.1 - Implementação C
  * ==============================================
- * 
- * Licença: MIT
- * Data: Novembro 2025
  */
 
 #include "tav.h"
