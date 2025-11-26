c0bc81f30d4eda5a0083ba9c278f40de72659767
Carlos Alberto Terencio de Bastos <caterencio@yahoo.com.br>
2025-11-26 20:04:05 -0300

    Update tav_arduino_example.ino

--- src/arduino/tav_arduino_example.ino
+++ src/arduino/tav_arduino_example.ino
@@ -1,4 +1,9 @@
 /*
+ * TAV Clock Cryptography v9.1
+ * Copyright (C) 2025 Carlos Alberto Terencio de Bastos
+ * License: AGPL-3.0 - https://github.com/caterencio/tav-crypto
+ */
+/*
  * TAV CRYPTO - Exemplo Arduino/ESP32
  * ===================================
  * 
