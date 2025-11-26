eb93289b38342d19db34861855f727e9276be5fe
Carlos Alberto Terencio de Bastos <caterencio@yahoo.com.br>
2025-11-26 19:56:07 -0300

    Update tav_openssl_engine.c

--- src/c/tav_openssl_engine.c
+++ src/c/tav_openssl_engine.c
@@ -1,4 +1,10 @@
 /*
+ * TAV Clock Cryptography v9.1
+ * Copyright (C) 2025 Carlos Alberto Terencio de Bastos
+ * License: AGPL-3.0 - https://github.com/caterencio/tav-crypto
+ */
+
+/*
  * TAV OPENSSL ENGINE
  * ==================
  * 
